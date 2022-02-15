8d5ee8ef3a46c5c2d71853ca42a8f4e48e46daf4
Josh H (santiago) <hendershotjoshr@gmail.com>
2022-02-14 22:22:28 -0400

    made world

--- main.py
+++ main.py
@@ -14,66 +14,72 @@ class Tile(Sprite):
         self.image = image
         self.rect = self.image.get_rect(topleft=(pos[0] * TILESIZE, pos[1] * TILESIZE))
 
+class World:
+    def __init__(self, size: Tuple[int]) -> None:
+        self.size = size
+        self.map_tiles = []
+    
+    def build(self):
+        bgs = [0, 1, 5]
+        for x in range(self.size[0]):
+            col = []
+            for y in range(self.size[1]):
+                col.append(choice(bgs))
+            self.map_tiles.append(col)
+    
+    def render(self, screen: py.Surface, sheet: list[py.Surface], topright = (0,0)):
+        width, height = (screen.get_width(), screen.get_height())
+        subset = [[col[y] for y in col[topright[1]:topright[1]+height]] for col in self.map_tiles[topright[0]:topright[0]+width]]
+        for x in range(width):
+            for y in range(height):
+                tile = sheet[subset[x][y]]
+                worldpos = (x*TILESIZE, y*TILESIZE)
+                rect = py.Rect(worldpos[0],worldpos[1],TILESIZE,TILESIZE)
+                screen.blit(tile, rect)
+
 class Game:
     def load_sprites(self):
-        sheet = SpriteSheet("images/1bitpack_kenney_1.2/Tilesheet/colored.png").load_grid_images(22, 49, x_padding=1, y_padding=1)
-        bgs = [sheet[0], sheet[1], sheet[2], sheet[5], sheet[6], sheet[7]]
-        
-        all_sprites: Group = Group()
-        
-        for x in range(WIDTH):
-            for y in range(HEIGHT):
-                Tile(choice(bgs), (x,y), all_sprites)
+        self.sheet = SpriteSheet("images/1bitpack_kenney_1.2/Tilesheet/colored.png").load_grid_images(22, 49, x_padding=1, y_padding=1)
 
-    def run(self):
-        py.init()
-        running = True
-        screen = py.display.set_mode((WIDTH, HEIGHT))
-
-        self.load_sprites()
-        
-        while running:
+    def game_loop(self):
+        campos_x, campos_y = (0,0)
+        while self.running:
             for event in py.event.get():
                 if event.type == py.QUIT:
-                    running = False
-                    break
+                    self.running = False
+                    return
+                elif event.type == py.KEYDOWN:
+                    if event.key == py.K_RIGHT:
+                        campos_x += 1
+                    elif event.key == py.K_LEFT:
+                        campos_x -= 1
+                    elif event.key == py.K_UP:
+                        campos_y -= 1
+                    elif event.key == py.K_DOWN:
+                        campos_y += 1
                 else:
                     print(event)
             
-            screen.fill((120,120,120))
-            all_sprites.draw(screen)
+            campos_x, campos_y = (min(campos_x, WIDTH), min(campos_y, HEIGHT))
+            campos_x, campos_y = (max(campos_x, 0), max(campos_y, 0))
+            self.screen.fill((120,120,120))
+            self.world.render(self.screen, self.sheet, (campos_x, campos_y))
             py.display.flip()
-        
+
+    def run(self):
+        py.init()
+        self.running = True
+        self.screen = py.display.set_mode((WIDTH, HEIGHT))
+
+        self.load_sprites()
+        self.world = World((1000,1000))
+        self.world.build()
+        self.game_loop()
         py.quit()
 
 def main():
-    py.init()
-    running = True
-    screen = py.display.set_mode((WIDTH, HEIGHT))
-
-    sheet = SpriteSheet("images/1bitpack_kenney_1.2/Tilesheet/colored.png").load_grid_images(22, 49, x_padding=1, y_padding=1)
-    bgs = [sheet[0], sheet[1], sheet[2], sheet[5], sheet[6], sheet[7]]
-    
-    all_sprites: Group = Group()
-    
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            Tile(choice(bgs), (x,y), all_sprites)
-    
-    while running:
-        for event in py.event.get():
-            if event.type == py.QUIT:
-                running = False
-                break
-            else:
-                print(event)
-        
-        screen.fill((120,120,120))
-        all_sprites.draw(screen)
-        py.display.flip()
-    
-    py.quit()
-            
+    game = Game()
+    game.run()        
 
 if __name__ == "__main__":
     main()
